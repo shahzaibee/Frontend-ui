ffd520754aa1ea8e61b18924ab6130dc5329f1da
Shahzaib <shahzaib1224@hotmail.com>
2024-01-16 12:28:38 +0500

    responsive issue

--- src/components/testinomial.tsx
+++ src/components/testinomial.tsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 
 const Testinomial = () => {
   return (
-    <div className="md:h-[350px] h-[350px] w-full bg-[#f6f8f5] mt-[20px] md:mt-0 overflow-x-hidden">
+    <div className="md:h-[350px]  pt-40 md:pt-0 h-[500px]  w-full bg-[#f6f8f5] mt-[20px] md:mt-0 overflow-x-hidden">
       <div className="flex flex-col justify-center gap-y-4 lg:py-4 items-center mx-auto md:p-10 h-[350px] w-[400px] md:w-[500px]">
         <div className="text-[#c5cdbf] flex flex-col justify-center items-center gap-y-5">
           <Image src={testiPic} alt="test" width={50} height={50} />
         </div>
         <div className="flex flex-col justify-center items-center mx-auto md:p-0 md:text-xl p-8">
-          <h1>
+          <h1 className="md:text-lg text-sm">
             “Original and with an innate understanding of their customer’s
             needs, the team at Love Nature are always a pleasure to work with.”
           </h1>
