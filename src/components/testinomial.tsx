583c9b55fae7689dda83cfc284ed23ef360bdfb3
Shahzaib <shahzaib1224@hotmail.com>
2024-01-16 12:33:25 +0500

    last responsive

--- src/components/testinomial.tsx
+++ src/components/testinomial.tsx
@@ -5,15 +5,16 @@ import Image from "next/image";
 
 const Testinomial = () => {
   return (
-    <div className="md:h-[350px]  pt-40 md:pt-0 h-[500px]  w-full bg-[#f6f8f5] mt-[20px] md:mt-0 overflow-x-hidden">
+    <div className="md:h-[350px]  pt-40 md:pt-0 h-[500px]   w-full bg-[#f6f8f5] mt-[100px] md:mt-0 overflow-x-hidden">
       <div className="flex flex-col justify-center gap-y-4 lg:py-4 items-center mx-auto md:p-10 h-[350px] w-[400px] md:w-[500px]">
         <div className="text-[#c5cdbf] flex flex-col justify-center items-center gap-y-5">
           <Image src={testiPic} alt="test" width={50} height={50} />
         </div>
         <div className="flex flex-col justify-center items-center mx-auto md:p-0 md:text-xl p-8">
           <h1 className="md:text-lg text-sm">
-            “Original and with an innate understanding of their customer’s
-            needs, the team at Love Nature are always a pleasure to work with.”
+            “Original and with an innate understanding <br /> of their
+            customer’s needs, the team at Love <br /> Nature are always a
+            pleasure to work with.”
           </h1>
         </div>
         <div className="flex flex-col justify-center items-center md:mt-5">
